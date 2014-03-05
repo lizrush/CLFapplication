4591729d72026dd3403c832f43d82ccb112dcff6
Aliya Rahman <aliya.rahman@yahoo.com>
2014-03-05 18:52:47 -0500

    Updated autopopulate script for sample data

--- cleanpopulate.py
+++ cleanpopulate.py
@@ -8,7 +8,8 @@
 #Note that syncdb ONLY adds new tables. Does not modify existing tables. SQLclear command isn't doing it either.
 
 import os
-from clfapplication.models import User, Recommendation, Evaluation
+from django.contrib.auth.models import User
+from clfapplication.models import Applicant, Recommender, Evaluator, Recommendation, Evaluation, Staff
 import csv
 
 
@@ -16,14 +17,24 @@ import csv
 ifile = open('clfsampledata.csv', "rb")
 reader = csv.reader(ifile)
 for row in reader:
+	u = User.objects.create_user(username = row[3], password = row[4],email = row[3])
+	u.save()
+	u = User.objects.get(email = row[3])
+	u.first_name = row[1]
+	u.last_name = row[2]
 	role = int(row[0])
-	if row[5]:
-		title = row[5]
-	else:
-		title = ""
-	if row[6]:
-		organization = row[6]
-	else:
-		organization=""
-	c = User(role=role, firstname = row[1], lastname = row[2], email = row[3], password = row[4], title = title, organization=organization)
-	c.save()
+	if role==1:
+		a = Applicant(user = u, role = 1)
+		a.save()
+	if role==2:
+		r = Recommender(user = u, role =2)
+		r.title = row[5]
+		r.organization = row[6]
+		r.save()
+	if role==3:
+		e = Evaluator(user = u, role =3)
+		e.save()
+	if role==4:
+		s = Staff(user = u, role = 4, title ="")
+		s.save()
+	u.save()
