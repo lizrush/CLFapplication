145216b4badce0dfabfb118770db6a55dbb7e720
Aliya Rahman <aliya.rahman@yahoo.com>
2014-03-06 00:34:45 -0500

    Database configuration update

--- chicanalatina/settings.py
+++ chicanalatina/settings.py
@@ -59,16 +59,8 @@ WSGI_APPLICATION = 'chicanalatina.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-if os.environ.has_key('DATABASE_URL'):
-    url = urlparse(os.environ['DATABASE_URL'])
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': url.path[1:],
-        'USER': url.username,
-        'PASSWORD': url.password,
-        'HOST': url.hostname,
-        'PORT': url.port,
-}
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
